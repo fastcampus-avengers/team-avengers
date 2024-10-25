d58f5b466be39ccf699e015b1fe4c56d6bb6d174
efforter <dlghdfhr12@naver.com>
2024-10-25 17:36:05 +0900

    fifififinal

--- hong_fizzbuzz.py
+++ hong_fizzbuzz.py
@@ -1,7 +1,1 @@
-for i in range(1,16):
-    if i%3 == 0:
-        print("fizz")
-    elif i%5 == 0:
-        print("buzz")
-    else:
-        print(i)
+[print("fizz"*(i%3==0) + "buzz"*(i%5==0) or i) for i in range(1,16)]
